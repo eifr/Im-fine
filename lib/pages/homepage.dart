8fa80240f9dff2ca3cbb80ee2bf48a5e7d21241e
eifr <eifr0980@gmail.com>
2023-10-13 11:55:35 +0300

    small fixes

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/contact.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:im_safe/components/im-safe-button.dart';
 import 'package:im_safe/main.dart';
